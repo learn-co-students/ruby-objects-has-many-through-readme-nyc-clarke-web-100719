dd68260704811100165ae3f95d9c87b70c736843
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-10-11 03:20:53 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,17 +17,27 @@ class Waiter
         Meal.new(self, customer, total, tip)
     end
 
+    def new_meal_20_percent(waiter, total)
+        tip = total * 0.2
+        Meal.new(waiter, self, total, tip)
+      end    
+
     def meals
-        Meal.all.select do |meal|
-            meal.waiter == self
-        end
+        Meal.all.select { |meal| meal.waiter == self }
     end
 
     def customers
-        meals.map do |meal|
-           howard meal.waiter = self
-        end
+        meals.map { |meal| meal.customer }
     end 
+
+    def tips
+        meals.map { |meal| meal.tip }
+    end
+
+    def avg_tips
+        tips.inject(&:+).to_f / tips.size
+
+    end
 
     def best_tipper
         best_tipped_meal = meals.max do |meal_a, meal_b|
@@ -37,5 +47,34 @@ class Waiter
         best_tipped_meal.customer
     end
       
+    def most_frequent_customer
+        customers.max_by { |customer| customers.count(customer) }
+    end
 
+    def worst_tipped_meal
+        meals.min_by { |meal| meal.tip }
+    end
+
+    def self.avg_tips_most_yrs
+        most_xp = self.all.max_by { |waiter| waiter.yrs_experience }
+        most_xp.avg_tips
+    end
+
+    def self.avg_tips_least_yrs
+        least_xp = self.all.min_by { |waiter| waiter.yrs_experience }
+        least_xp.avg_tips
+    end
+
+    def self.oldest_customer
+        oldest_age = 0
+        oldest_customer = nil
+        self.all.each do |customer|
+          if customer.age > oldest_age
+            oldest_age = customer.age
+            oldest_customer = customer
+          end
+        end
+        oldest_customer
+      end
+    
 end
